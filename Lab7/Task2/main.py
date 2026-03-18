b55acff37c7803201acd0f46db972afd80134099
Nizhat <nijat.2105@gmail.com>
2026-03-18 15:05:27 +0500

    re lab7

--- Lab7/Task2/main.py
+++ Lab7/Task2/main.py
@@ -1,17 +1,15 @@
 from models import Animal, Dog, Cat
 
 def main():
-    # создаём объекты
+
     animal = Animal("Generic", 5, "gray")
     dog = Dog("Rex", 3, "brown", "Shepherd")
     cat = Cat("Murka", 2, "white", 9)
 
-    # список объектов
     animals = [animal, dog, cat]
 
-    # проходимся по списку
     for a in animals:
-        print(a)              # вызывает __str__
+        print(a)              # вызывает str
         print(a.info())       # общий метод
         print(a.speak())      # полиморфизм
         print("------")
@@ -20,7 +18,5 @@ def main():
     print(dog.fetch())
     print(cat.scratch())
 
-
-# запуск программы
 if __name__ == "__main__":
     main()
